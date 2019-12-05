a8e9a86803ea8319028b181f48d5f473fb2b9462
danieldagot <danieldagot@gmail.com>
2019-12-05 15:11:42 +0200

    setting up to push to Heroku

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
+// const { ApolloServer, gql } = require("apollo-server-express");
+// //const port = process.env.PORT
+// const path = require("path");
+// //require('dotenv').config()
+// let port = 4000;
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const resolvers = require("./src/resolvers");
+// const typeDefs = require("./src/typeDefs");
+// const express = require("express")
+
+// const startServer = async () => {
+//   const app = express();
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers
+//   });
+
+//   server.applyMiddleware({ app });
+//   await mongoose.connect("mongodb://localhost:27017/test3", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology:true
+//   });
+
+//   app.listen({ port: port }, () =>
+//     console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+//   );
+// };
+
+// startServer();
+
+const express = require('express');
+const app = express();
 const { ApolloServer, gql } = require("apollo-server-express");
 //const port = process.env.PORT
 const path = require("path");
-require('dotenv').config()
-let port = process.env.PORT || 4000;
+let port = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const resolvers = require("./src/resolvers");
 const typeDefs = require("./src/typeDefs");
-const express = require("express")
+const cors = require('cors');
+
+
+app.use(cors({
+  origin: function (origin, callback) {
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+          var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  }
+}));
+
+
 
 const startServer = async () => {
   const app = express();
@@ -18,13 +67,16 @@ const startServer = async () => {
 
   server.applyMiddleware({ app });
 
-  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name', {
+  await mongoose.connect("mongodb://localhost:27017/test3", {
     useNewUrlParser: true,
     useUnifiedTopology:true
   });
-  app.listen({ port: port }, () =>
-    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+  app.use('/', (req, res) => res.send("pace go to /graphql"))
+  app.listen({ port: 4000 }, () =>
+  
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   );
 };
+
 
 startServer();
